e779da6f143c4b643266e967cdad60acee2debf5
WereWind <serg-ef@mail.ru>
2017-10-13 17:21:13 +0300

    Clean mainwindow header

--- mainwindow.h
+++ mainwindow.h
@@ -18,24 +18,8 @@ class MainWindow : public QMainWindow {
   void ReorderText();
 
  private:
-  static int MethodParamsAmount(QString truncated_method);
-  static int MethodStringAmount(QString truncated_method);
-  static QString TruncateCommentsFromMethod(const QString &method);
   static QString CleanString(const QString &string);
 
-  static bool SortingForMethods(const QString &left_method,
-                                const QString &right_method);
-
-  QString ReorganizeSection(const QString &code_section);
-  QStringList SplitSectionIntoMethods(const QString &code_section);
-  void PlaceMethodsIntoGroups(const QStringList &methods);
-  void AddStringIntoListOfLists(int list_index, const QString &string);
-  void SortMethodsInGroups();
-  QString AssembleSortedString();
-
-  QString FindClassBlocksInString(QString &block);
-
   Ui::MainWindow *ui;
-  QString input_string_;
 };
 #endif  // MAINWINDOW_H
